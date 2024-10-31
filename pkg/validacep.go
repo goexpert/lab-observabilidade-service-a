3a20faa8601d7c2dc174957e03da5d9b241c7847
Rogerio Tadim <rogeriotadim@gmail.com>
2024-10-31 02:14:07 +0000

    app: check len cep == 8

--- pkg/validacep.go
+++ pkg/validacep.go
@@ -6,11 +6,18 @@ import (
 )
 
 func ValidaCEP(cepInput json.RawMessage) error {
-	var cepNum float64
 	if !json.Valid(cepInput) {
 		return errors.New("invalid json for cep")
 	}
+	var cepNum float64
 	if err := json.Unmarshal(cepInput, &cepNum); err == nil {
+		return errors.New("invalid zipcode")
+	}
+	var cepStr string
+	if err := json.Unmarshal(cepInput, &cepStr); err != nil {
+		return errors.New("invalid zipcode")
+	}
+	if len(cepStr) != 8 {
 		return errors.New("invalid zipcode")
 	}
 	return nil
