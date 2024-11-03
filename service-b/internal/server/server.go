4be6ed1ac0b8ac5059ced9e51f42b1d04c15a859
Rogerio Tadim <rogeriotadim@gmail.com>
2024-11-03 19:14:12 -0300

    fix: include otel

--- service-b/internal/server/server.go
+++ service-b/internal/server/server.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
@@ -14,8 +16,11 @@ type Server struct {
 	port int
 }
 
-func NewServer() *http.Server {
-	port, _ := strconv.Atoi(os.Getenv("PORT"))
+func NewServer(ctx context.Context) (*http.Server, error) {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		return nil, err
+	}
 	NewServer := &Server{
 		port: port,
 	}
@@ -23,11 +28,12 @@ func NewServer() *http.Server {
 	// Declare Server config
 	server := &http.Server{
 		Addr:         fmt.Sprintf(":%d", NewServer.port),
+		BaseContext:  func(_ net.Listener) context.Context { return ctx },
 		Handler:      NewServer.RegisterRoutes(),
 		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
+		ReadTimeout:  2 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	return server
+	return server, nil
 }
